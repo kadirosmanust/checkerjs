c57eda0d33f0d3dadae91ae67271ba0d6356533e
KadoRaw <92394525+KadoRaw@users.noreply.github.com>
2022-10-17 16:25:17 +0300

    .

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import isArray from "./Array/isArray";
-import isAnyNullorUndefined from "./Array/isAnyNullorUndefined";
-import isInRange from "./Array/isInRange";
-import isAnyKeyHasFreez from "./Object/isAnyKeyHasFreez";
-import deepEquality from "./Object/deepEquality";
-import isOdd from "./Number/isOdd";
-export { isArray, isAnyNullorUndefined, isInRange, deepEquality, isAnyKeyHasFreez, isOdd };
