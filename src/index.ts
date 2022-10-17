0c0650c95fa42c193ab551c17387a1deddff5ce2
Atılay Köşker <atilay@thekompanion.com>
2022-10-17 14:12:29 +0300

    feat: add isOdd checker

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,7 @@ import isInRange from "./Array/isInRange";
 // Object
 import isAnyKeyHasFreez from "./Object/isAnyKeyHasFreez";
 import deepEquality from "./Object/deepEquality";
+// Number
+import isOdd from "./Number/isOdd";
 
-export { isArray, isAnyNullorUndefined, isInRange, deepEquality, isAnyKeyHasFreez };
+export { isArray, isAnyNullorUndefined, isInRange, deepEquality, isAnyKeyHasFreez,isOdd };
