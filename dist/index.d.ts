845a3748c1200c63950e0385a3a978441cf5031c
KadoRaw <92394525+KadoRaw@users.noreply.github.com>
2022-10-17 16:09:43 +0300

    YAML changes

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,4 +3,5 @@ import isAnyNullorUndefined from "./Array/isAnyNullorUndefined";
 import isInRange from "./Array/isInRange";
 import isAnyKeyHasFreez from "./Object/isAnyKeyHasFreez";
 import deepEquality from "./Object/deepEquality";
-export { isArray, isAnyNullorUndefined, isInRange, deepEquality, isAnyKeyHasFreez };
+import isOdd from "./Number/isOdd";
+export { isArray, isAnyNullorUndefined, isInRange, deepEquality, isAnyKeyHasFreez, isOdd };
